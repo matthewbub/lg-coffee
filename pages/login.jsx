836588569f03d23f7cfb6dee830c4b0c4b4343e2
hi-matbub <6matbub@gmail.com>
2020-11-02 15:19:25 -0800

    fix: import err

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,4 +1,4 @@
-import { LogInForm } from "../components/LogInForm";
+import { LogInForm } from "../components/log_in_form.jsx";
 import { Header, Footer, FormContainer } from "../components/layout";
 
 const LoginPage = () => {
