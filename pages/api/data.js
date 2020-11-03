0f8039213e56de7c48fb4d9c6a7f962df64c83ff
hi-matbub <6matbub@gmail.com>
2020-11-03 05:37:55 -0800

    feat: responsive product page

--- pages/api/data.js
+++ pages/api/data.js
@@ -3,8 +3,5 @@ import { data } from "../../utils/data";
 export default function handler(req, res) {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/json");
-
-  // fake db call
-
   return res.end(JSON.stringify(data));
 }
--- pages/view-product/[pid].jsx
+++ pages/view-product/[pid].jsx
@@ -1,22 +1,75 @@
 import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import { Header, Footer } from "../../components/layout";
 
 const ViewProduct = (props) => {
   const router = useRouter();
   const { pid } = router.query;
-
+  const [product, setProduct] = useState(null);
   const products = props.data.products;
-  let product;
-  for (let i = 0; i < products.length; i++) {
-    Object.keys(products[i]).map((y) =>
-      products[i].productSku === "/" + pid
-        ? (product = products[i])
-        : "Whoops Looks like that product isn't avaiable right now."
+
+  // for (let i = 0; i < products.length; i++) {
+  //   Object.keys(products[i]).map((y) =>
+  //     products[i].productSku === "/" + pid
+  //       ? useEffect(() => {
+  //          return setProduct(products[i])
+  //         })
+  //       : "Whoops Looks like that product isn't avaiable right now."
+  //   );
+  // }
+
+  useEffect(() => {
+    for (let i = 0; i < products.length; i++) {
+      Object.keys(products[i]).map((y) =>
+        products[i].productSku === "/" + pid
+          ? setProduct(products[i])
+          : "Whoops Looks like that product isn't avaiable right now."
+      );
+    }
+  });
+
+  if (product) {
+    return (
+      <div>
+        <Header data={props.data} />
+        <div className="fluid-container bg-light pt-5 pb-5">
+          <div className="container">
+            <div className="row">
+              <section className="col-md-6 col-sm-12 bg-light pid_section">
+                <img
+                  className="main_img"
+                  src={product.productImages[0]}
+                  alt={"product image"}
+                />
+                <div className="img_previews">
+                  {product.productImages.map((i) => (
+                    <button className="img_preview_button">
+                      <img src={i} alt={"product image"} />
+                    </button>
+                  ))}
+                </div>
+              </section>
+              <section className="col-md-6 col-sm-12 bg-light pid_section">
+                <div className="p-sm-3">
+                  <h2 className="product_view_title">{product.productTitle}</h2>
+                  <p className="product_view_description">
+                    {product.productDescription}
+                  </p>
+                </div>
+                <div className="align-self-end p-sm-3 d-flex flex-column align-items-end">
+                  <p className="product_view_price">{product.productPrice}</p>
+                  <button className="btn btn-outline-dark">Add To Cart</button>
+                </div>
+              </section>
+            </div>
+          </div>
+        </div>
+        <Footer data={props.data} />
+      </div>
     );
+  } else {
+    return <div>Loading...</div>;
   }
-
-  console.log(product);
-
-  return <p>Post: {pid}</p>;
 };
 
 export default ViewProduct;
