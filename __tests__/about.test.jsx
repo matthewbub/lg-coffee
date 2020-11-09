60d98967bdbd24d81ec8eeb489608398aa429acc
hi-matbub <6matbub@gmail.com>
2020-11-09 06:45:58 -0800

    fix: updated snapshots

--- __tests__/about.test.jsx
+++ __tests__/about.test.jsx
@@ -4,15 +4,15 @@ import renderer from 'react-test-renderer';
 import About from '../pages/info/about';
 
 // needs to fetch data first
-// import { data } from '../utils/data';
+import { data } from '../utils/data';
 
 it('renders smoke test without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<About />, div);
+  ReactDOM.render(<About data={data} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders homepage unchanged', () => {
-  const tree = renderer.create(<About />).toJSON();
+it('renders about page unchanged', () => {
+  const tree = renderer.create(<About data={data} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
