2137dda10952306150757b3af6b5edb9bbfd05c4
hi-matbub <6matbub@gmail.com>
2020-11-19 05:42:43 -0800

    test: updating snapshots

--- packages/lg-init/lib/templates/og/pages/_app.jsx
+++ packages/lg-init/lib/templates/og/pages/_app.jsx
@@ -35,9 +35,10 @@ function App({ Component, pageProps }) {
   return (
     <StripeWrapper>
       <Head>
-        <title>{data.StoreName}</title>
+        <title>{data.name}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />        
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800;900&display=swap"
           rel="stylesheet"
